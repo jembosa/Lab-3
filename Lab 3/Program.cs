1b06dd8f558a1e68d91e4b9509f40a5c88c24062
Alex Young <alexanderjyoung15@gmail.com>
2023-10-04 20:32:09 -0400

    formated table, cleaned math up

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -10,16 +10,29 @@ namespace Lab_3
             int i;
             DataTable MyTable = new DataTable(); // 1
             DataTable MyTableByName = new DataTable("MyTableName"); // 2
+            string stoploop = "";
 
-            Console.WriteLine("Please enter a number between 1 and 100");          
-                double n = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Number         Squared        Cubed");
-            Console.WriteLine("========       =======       =======");
-            for (i = 1; i <= n; i++)
+            while (stoploop == "")
             {
-                Console.WriteLine(i);
-                Console.WriteLine(FindSquare(i));
-                Console.WriteLine(FindCube(i));           
+                Console.WriteLine("Please enter a number between 1 and 100");
+                double n = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Number" + "\t" + "Squared" + "\t" + "Cubed");
+                Console.WriteLine("======" + "\t" + "=======" + "\t" + "=====");
+                for (i = 1; i <= n; i++)
+                {
+                    Console.WriteLine(i + "\t" + FindSquare(i) + "\t" + FindCube(i));
+                    //Console.WriteLine(FindSquare(i));
+                    //Console.WriteLine(FindCube(i));
+                }
+
+                //ask to stop here
+                Console.WriteLine("Type 'stop' to exit or press 'Enter' to try again.");
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "stop")
+                {
+                    break;
+
+                }
             }
         }
         public static double FindSquare(double n)
